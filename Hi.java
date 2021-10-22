bae1a39dca9d6ffb2e5a8a2092919d7259787387
bala74923 <bala74923@gmail.com>
2021-10-22 20:05:07 +0530

    main branch modified

---
+++ Hi.java
@@ -0,0 +1,12 @@
+package curr;
+import java.uti.Scanner;
+class Hi
+{
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		String  name = sc.next();
+		System.out.println("Hi "+name);
+		sc.close();
+	}
+}
